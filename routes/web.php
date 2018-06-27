0be53045771791bc915f34bc5c8f497c50bcf6a4
mylovetron <khanhnd.fg@vietsov.com.vn>
2018-06-27 15:56:16 +0700

    vsp 27-6

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('loai_san_pham/{id}',['as'=>'loaisanpham','uses'=>'WelcomeController@loaisanpham']);
 Route::get('chi-tiet-san-pham/{id}',['as'=>'chitietsanpham','uses'=>'WelcomeController@chitietsanpham']);
+Route::get('mua-hang/{id}/{tensanpham}',['as'=>'muahang','uses'=>'WelcomeController@muahang']);
