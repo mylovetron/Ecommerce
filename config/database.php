048eb83f0282936996ec928a473f60bfdd5d7839
mylovetron <khanhnd.fg@vietsov.com.vn>
2018-10-24 14:18:35 +0700

    24-10-6

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql12261711'),
+            'username' => env('DB_USERNAME', 'sql12261711'),
+            'password' => env('DB_PASSWORD', 'mjWfMDbrSE'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
